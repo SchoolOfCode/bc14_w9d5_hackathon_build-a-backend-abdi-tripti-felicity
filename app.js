0b98d913801470f69463a60b684c7b7d0e13477b
abdirahmanmohamed <abdi3332@outlook.com>
2023-05-12 10:20:48 +0100

    in the middle of implementing create recipe

--- app.js
+++ app.js
@@ -10,10 +10,29 @@ import {
 
 const app = express();
 const PORT = 3000;
+let allRecipes = await getRecipes()
 
 app.use(express.static("public"));
 app.use(express.json());
 
-app.listen(PORT, () => {
+app.get("/api/recipes", async (req,res) => {
+  let allRecipes = await getRecipes()
+  res.json(allRecipes)
+  console.log(allRecipes)
+})
+
+app.get("/api/recipes/:id", async (req,res) => {
+let recipeID = req.params.id
+let recipe = await getRecipeByID(recipeID)
+res.json(recipe)
+})
+
+app.post("/api/recipes", async (req,res) =>{
+ let addedRecipe = await createRecipe() 
+ res.json(addedRecipe)
+ console.log(req.body)
+})
+
+  app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
--- recipes.js
+++ recipes.js
@@ -2,15 +2,35 @@ import fs from "node:fs/promises";
 import { v4 as uuidv4 } from "uuid";
 
 const fileName = "recipes.json";
-
+let newRecipe = {
+    "id": "4c848d48-b81e-4d6f-b45d-7b3090f4f8eu",
+    "title": "Kellogs Cornflakes",
+    "ingredients": ["50g of cornflakes", "100ml of milk"],
+    "instructions": "Put the cornflakes in a bowl, add the milk. Eat with a spoon.",
+    "image": "https://upload.wikimedia.org/wikipedia/commons/c/c3/Kellogg%27s_Corn_Flakes%2C_with_milk.jpg"
+  }
 // GET ALL RECIPES
-export async function getRecipes() {}
+export async function getRecipes() {
+    let Data = await fs.readFile(fileName, "utf8");
+    let recipes = JSON.parse(Data);
+    return recipes;
+
+}
 
 // GET A RECIPE BY ID
-export async function getRecipeByID(id) {}
+export async function getRecipeByID(id) {
+    let recipes = await getRecipes();
+    let recipe = recipes.find((recipe) => recipe.id === id);
+    return recipe;
+}
 
 // CREATE A RECIPE
-export async function createRecipe(newRecipe) {}
+export async function createRecipe(newRecipe) {
+    let recipes = await getRecipes();
+    recipes.push(newRecipe)
+    await fs.writeFile(fileName, JSON.stringify(recipes))
+    return newRecipe;
+}
 
 // UPDATE A RECIPE BY ID
 export async function updateRecipeByID(id, updatedRecipe) {}
